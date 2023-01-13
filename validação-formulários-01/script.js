564aea7360b7b8120a8634fbf830e710146fcd0b
Francisco Cruz <fcocruz1107@gmail.com>
2023-01-12 21:28:06 -0300

    3ª etapa da atividade

--- validação-formulários-01/script.js
+++ validação-formulários-01/script.js
@@ -14,3 +14,47 @@ usernameInput.addEventListener("blur", function(){
 })
 
 // Validar valor do input
+usernameInput.addEventListener("change",(e)=>{
+ let valor =  e.target.value
+ 
+if(valor.length < 3){
+
+   usernameInput.classList.add("error")
+   usernameInput.classList.remove("correct")
+   usernameHelper.innerText = "Seu user name precisa ter pelo menos 3 caracteres"
+   usernameHelper.classList.add("visible")
+}else{
+ 
+    usernameInput.classList.add("correct")
+ usernameInput.classList.remove("error")
+ usernameHelper.classList.remove("visible")
+}
+
+})
+
+let emailInput = document.getElementById("email");
+let emailLabel = document.querySelector('label[for="email"]');
+let emailHelper = document.getElementById("email-helper");
+
+
+emailInput.addEventListener("change",(e)=>{
+let valor = e.target.value
+console.log(valor.includes(".com"))
+
+if(valor.includes("@") && valor.includes(".com")){
+    emailInput.classList.remove("error")
+ emailInput.classList.add("correct")  
+ emailHelper.innerText = "Email incorreto precisa ter @ e .com"
+ emailHelper.classList.remove("visible")
+
+    
+ }else{
+    emailInput.classList.remove("correct")   
+    emailInput.classList.add("error")
+    emailHelper.classList.add("visible")
+   
+ }
+ 
+ })
+
+ 
